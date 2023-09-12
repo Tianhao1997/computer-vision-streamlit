c5b40e7229c1a7c483d57dc9b634e9384b166513
Tianhao1997 <84186537+Tianhao1997@users.noreply.github.com>
2023-09-12 23:00:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -73,6 +73,112 @@ hand_gesture_classifier = load_hand_gesture_classifier()
 image_optical_character_recognition = load_image_optical_character_recognition()
 image_classifier = load_image_classifier()
 
+
+#############################################
+@st.cache(allow_output_mutation=True)
+def gesture_classification(frame):
+    """
+    Perform hand detection, classification inference, and annotate frame.
+
+    Parameters:
+        frame (av.VideoFrame): frame from webcam
+    Returns:
+        frame (av.VideoFrame): annotated video frame
+    """
+    x, y, c = frame.shape
+
+    # Flip the frame vertically
+    frame = cv2.flip(frame, 1)
+    framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # Get hand landmark prediction
+    result = self.hands.process(framergb)
+
+    className = ''
+
+    # post process the result
+    if result.multi_hand_landmarks:
+        landmarks = []
+        for handslms in result.multi_hand_landmarks:
+            for lm in handslms.landmark:
+                lmx = int(lm.x * x)
+                lmy = int(lm.y * y)
+
+                landmarks.extend([lmx, lmy])
+
+            # Drawing landmarks on frames
+            self.mpDraw.draw_landmarks(frame, handslms, self.mpHands.HAND_CONNECTIONS,
+                                       self.mpDraw.DrawingSpec(color=(48, 255, 48), thickness=2, circle_radius=3),
+                                       self.mpDraw.DrawingSpec(color=(255, 255, 255), thickness=2, circle_radius=3))
+
+            # Predict gesture
+            prediction = model_xgb.predict(np.array(landmarks).reshape(1, -1))
+            predicted_names = [k for k, v in gesture_names.items() if v == prediction]
+            className = str(predicted_names[0])
+
+            # Get text cooorinates
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            fontScale = 1
+            text_width, text_height = cv2.getTextSize(className, font, fontScale, cv2.LINE_AA)[0]
+            CenterCoordinates = (int(frame.shape[1] / 2) - int(text_width / 2) - 25, 50)
+
+            # show the prediction on the frame
+            cv2.putText(frame, className, CenterCoordinates, cv2.FONT_HERSHEY_SIMPLEX,
+                        1, (48, 255, 48), 2, cv2.LINE_AA)
+    else:
+        text = "Waiting for hand gesture..."
+
+        # show the prediction on the frame
+        cv2.putText(frame, text, (75, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1, cv2.LINE_AA)
+
+    return frame
+
+def callback(frame: av.VideoFrame) -> av.VideoFrame:
+    """
+    Callback for hand gesture classification through webcam.
+
+    Parameters:
+        frame (av.VideoFrame): video frame taken from webcam
+    Returns:
+        annotated_frame (av.VideoFrame): video frame with annotations included
+    """
+
+    image = frame.to_ndarray(format="bgr24")
+    annotated_image = gesture_classification(image)
+
+    return av.VideoFrame.from_ndarray(annotated_image, format="bgr24")
+
+
+model_xgb = XGBClassifier()
+model_xgb.load_model("model.json")
+gesture_names = {'A': 0,
+                 'B': 1,
+                 'C': 2,
+                 'D': 3,
+                 'E': 4,
+                 'F': 5,
+                 'G': 6,
+                 'H': 7,
+                 'I': 8,
+                 'J': 9,
+                 'K': 10,
+                 'L': 11,
+                 'M': 12,
+                 'N': 13,
+                 'O': 14,
+                 'P': 15,
+                 'Q': 16,
+                 'R': 17,
+                 'S': 18,
+                 'T': 19,
+                 'U': 20,
+                 'V': 21,
+                 'W': 22,
+                 'X': 23,
+                 'Y': 24,
+                 'Z': 25}
+#################################################################
+
 # Paths for image examples
 image_examples = {'Traffic': 'examples/Traffic.jpeg',
                   'Barbeque': 'examples/Barbeque.jpeg',
@@ -425,11 +531,20 @@ elif page == 'Hand Gesture Classification':
     RTC_CONFIGURATION = RTCConfiguration(
         {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
     )
+    # webrtc_ctx = webrtc_streamer(
+    #     key="hand-gesture-classification",
+    #     mode=WebRtcMode.SENDRECV,
+    #     rtc_configuration=RTC_CONFIGURATION,
+    #     video_frame_callback=hand_gesture_classifier.callback,
+    #     media_stream_constraints={"video": True, "audio": False},
+    #     async_processing=True,
+    # )
+
     webrtc_ctx = webrtc_streamer(
         key="hand-gesture-classification",
         mode=WebRtcMode.SENDRECV,
         rtc_configuration=RTC_CONFIGURATION,
-        video_frame_callback=hand_gesture_classifier.callback,
+        video_frame_callback=callback,
         media_stream_constraints={"video": True, "audio": False},
         async_processing=True,
     )
